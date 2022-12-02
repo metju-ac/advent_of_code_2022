3fe2c663d1ccf83350bce35e2a0d36576abaa39e
Matej Klima <matej.klima5@gmail.com>
2022-12-02 06:38:22 +0100

    Day two

--- 02/02.py
+++ 02/02.py
@@ -1,19 +1,48 @@
 def load_data():
     data = []
     with open("02.txt", "r") as f:
-        cur_elf = 0
         for line in f:
-            data.append(int(line.strip()))
-
+            data.append(line.strip().split(" "))
     return data
 
 
+def first_score(elf, me):
+    if ord(elf) + 23 == ord(me):
+        return 3
+    if (elf == "A" and me == "Y") or \
+       (elf == "B" and me == "Z") or \
+       (elf == "C" and me == "X"):
+        return 6
+    return 0
+
+
 def first(data):
-    pass
+    score  = 0
+    for turn in data:
+        elf, me = turn[0], turn[1]
+        score += first_score(elf, me)
+        score += ord(me) - 87
+    return score
+
+
+def second_score(elf, win):
+    if win == "X":
+        if elf == "A":
+            return 3
+        return ord(elf) - 65
+    if win == "Y":
+        return ord(elf) - 61
+    if elf in ("A", "B"):
+        return ord(elf) - 57
+    return 7
 
 
 def second(data):
-    pass
+    score = 0
+    for turn in data:
+        elf, me = turn[0], turn[1]
+        score += second_score(elf, me)
+    return score
 
 
 if __name__ == '__main__':
