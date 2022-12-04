42e833fb7f22ef5aff619433149af4d4fd06acfb
Matej Klima <matej.klima5@gmail.com>
2022-12-04 08:32:04 +0100

    Ready for day 5

--- 04/04.py
+++ 04/04.py
@@ -2,32 +2,31 @@ def load_data():
     data = []
     with open("04.txt", "r") as f:
         for line in f:
-            line = line.strip().split(",")
-            line[0] = line[0].split("-")
-            line[1] = line[1].split("-")
-            for i in range(len(line)):
-                line[i] = list(map(int, line[i]))
+            line = line.strip().replace(",", "-").split("-")
+            line = list(map(int, line))
             data.append(line)
     return data
 
+
 def first(data):
     count = 0
-    for fst, snd in data:
-        if (fst[0] <= snd[0] and fst[1] >= snd[1]) \
-        or (snd[0] <= fst[0] and snd[1] >= fst[1]):
+    for low_1, high_1, low_2, high_2 in data:
+        if (low_1 <= low_2 and high_1 >= high_2) \
+                or (low_2 <= low_1 and high_2 >= high_1):
             count += 1
     return count
 
 
 def second(data):
     count = 0
-    for fst, snd in data:
-        if (fst[0] <= snd[1] <= fst[1]) or (snd[0] <= fst[1] <= snd[1]):
+    for low_1, high_1, low_2, high_2 in data:
+        if low_2 <= high_1 and low_1 <= high_2:
             count += 1
     return count
 
 
 if __name__ == '__main__':
     data = load_data()
+    print(data)
     print(first(data))
     print(second(data))
