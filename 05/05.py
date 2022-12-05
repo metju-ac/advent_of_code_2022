bc1d793b3deb11470a26d04d38786c5e00041410
Matej Klima <matej.klima5@gmail.com>
2022-12-05 11:54:34 +0100

    Day 5

--- 05/05.py
+++ 05/05.py
@@ -1,19 +1,56 @@
+from collections import deque, defaultdict
+from copy import deepcopy
+
+
 def load_data():
-    data = []
     with open("05.txt", "r") as f:
-        for line in f:
-            data.append(line.strip())
-    return data
-
-def first(data):
-    pass
+        lines = f.read()
+        data = lines.split("\n\n")
+    return parse_stacks(data[0]), parse_instructions(data[1])
 
 
-def second(data):
-    pass
+def parse_stacks(stacks):
+    dic = defaultdict(deque)
+    for line in stacks.split("\n"):
+        for i, j in enumerate(range(0, len(line), 4)):
+            char = line[j + 1]
+            if char.isalpha():
+                dic[i + 1].appendleft(char)
+    return dic
+
+
+def parse_instructions(instructions):
+    res = []
+    for line in instructions.split("\n"):
+        line = line.split(" ")
+        res.append((int(line[1]), int(line[3]), int(line[5])))
+    return res
+
+
+def get_result(dic):
+    res = ""
+    for i in range(len(dic)):
+        res += dic[i + 1].pop()
+    return res
+
+
+def first(dic, instructions):
+    for amount, fr, to in instructions:
+        for _ in range(amount):
+            dic[to].append(dic[fr].pop())
+    return get_result(dic)
+
+
+def second(dic, instructions):
+    for amount, fr, to in instructions:
+        picked = ""
+        for _ in range(amount):
+            picked += dic[fr].pop()
+        dic[to].extend(reversed(picked))
+    return get_result(dic)
 
 
 if __name__ == '__main__':
-    data = load_data()
-    print(first(data))
-    print(second(data))
+    sta, ins = load_data()
+    print(first(deepcopy(sta), ins))
+    print(second(sta, ins))
---
+++ 06/06.py
@@ -0,0 +1,19 @@
+def load_data():
+    data = []
+    with open("06.txt", "r") as f:
+        for line in f:
+            data.append(line.strip())
+    return data
+
+def first(data):
+    pass
+
+
+def second(data):
+    pass
+
+
+if __name__ == '__main__':
+    data = load_data()
+    print(first(data))
+    print(second(data))
